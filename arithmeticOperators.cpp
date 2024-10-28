cf27445b7abd4cdd88b15b989ca96980ade4e3ec
joycse2005 <joybiswas2028@gmail.com>
2024-10-28 13:48:08 +0600

    first commit

---
+++ arithmeticOperators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=7,y=2;
+    cout<<x+y<<endl;//9
+    cout<<x-y<<endl;//5
+    cout<<x*y<<endl;//14
+    cout<<x/y<<endl;//3.5 tobe 3 ase
+
+    float x=3.14,y=2.46;
+    cout<<x+y<<endl;//5.6
+    cout<<x-y<<endl;//0.68
+    cout<<x*y<<endl;//7.7244
+    cout<<x/y<<endl;//1.27642
+
+    float x=7,y=2;
+    cout<<x+y<<endl;//9
+    cout<<x-y<<endl;//5
+    cout<<x*y<<endl;//14
+    cout<<x/y<<endl;//3.5
+}
